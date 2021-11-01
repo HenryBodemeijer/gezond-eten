c0fe132eb1d45c296605e29521d6213f0ae6abd6
henry <89969376+HenryBodemeijer@users.noreply.github.com>
2021-11-01 22:06:45 +0100

    Add files via upload

---
+++ gezond-eten-main/vragenlijst.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+        <ul class="navbar">
+            <img id="logo" src="Afbeeldingen/logo.png" alt="logo">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="recepten.html">recepten</a></li>
+            <li><a href="Bmi.html">BMI calc.</a></li>
+            <li><a href="informatie.html">Informatie</a></li>
+            <li><a href="vragenlijst.html" class="active">vragenlijst</a></li>
+            
+        </ul>
+    </div>
+    <div id="vragenlijstresultaten">
+        <h1>Vragenlijst resultaten</h1>
+    <?php
+
+    
+
+    ?>
+    </div>
+    
+</body>
+<footer>
+    <ul class="terug">
+        <li><a href="vragenlijst.html">Terug</a></li>
+    </ul>
+</footer>
+</html>
