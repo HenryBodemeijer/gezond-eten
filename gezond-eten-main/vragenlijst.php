bdeb5a74b1a03846f6e1e77944c16bec8fad9bf9
henry <89969376+HenryBodemeijer@users.noreply.github.com>
2021-11-03 10:00:15 +0100

    Add files via upload

--- gezond-eten-main/vragenlijst.php
+++ gezond-eten-main/vragenlijst.php
@@ -21,11 +21,44 @@
     </div>
     <div id="vragenlijstresultaten">
         <h1>Vragenlijst resultaten</h1>
-    <?php
-
     
 
-    ?>
+    <?php
+$fruit = $_POST['fruit'];
+$kantine = $_POST['kantine'];
+$etenkantine = $_POST['etenkantine'];
+$frisdrank = $_POST['frisdrank'];
+$ontbijt = $_POST['ontbijt'];
+$eten = $_POST['eten'];
+$bmi = $_POST['bmi'];
+$score = $fruit + $kantine + $etenkantine + $frisdrank + $ontbijt + $eten + $bmi;
+
+
+
+
+echo"Bedankt voor het invoeren van de vragenlijst.<br> u score van  $score <br>";
+if($score == 7){
+    echo"Dat is het laagste mogelijk dat je kan krijgen. <br> U bent erg ongezond";
+}
+
+if($score >7 && $score <12){
+    echo"Dat is een erge lage score. <br> U bent erg ongezond u kan beter wat gezonder gaan leven";
+}
+
+if($score >11 && $score <18){
+    echo"U score is erg gemmiddeld. <br> U bent niet erg gezond, maar ook niet erg ongezond u kunt nog altijd gezonder gaan leven";
+}
+
+if($score >17 && $score <21){
+    echo"U heeft een erg hoge score. <br> U heeft bijna het hoogst. <br> U bent best gezond ga zo door.";
+}
+
+if($score == 21){
+    echo"U heeft de allerhoogste score. <br> u bent erg gezond blijf zo leven en dan blijf je gezond";
+}
+
+?>
+
     </div>
     
 </body>
@@ -35,3 +68,4 @@
     </ul>
 </footer>
 </html>
+
